d0eaf20b0001fc451ec6f53d56ff1e5c3bc4e35f
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-09 15:34:22 +0100

    task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A simple flask app."""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/,', strict_slashes=False)
+def home():
+    """A route to the homepage of the application."""
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
